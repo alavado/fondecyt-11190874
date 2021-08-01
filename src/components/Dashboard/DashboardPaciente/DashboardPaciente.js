77435d9b324d34eeed4d12e3689632757cb95bf9
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-08-01 18:43:05 -0400

    agrega boton para copiar link

--- src/components/Dashboard/DashboardPaciente/DashboardPaciente.js
+++ src/components/Dashboard/DashboardPaciente/DashboardPaciente.js
@@ -5,11 +5,21 @@ import Intoxicacion from './Intoxicacion'
 import Riesgos from './Riesgos'
 import CryptoJS from 'crypto-js'
 import logo from '../../../assets/TMB Color-01.svg'
+import { InlineIcon } from '@iconify/react'
+import iconoCopiar from '@iconify-icons/mdi/content-copy'
+import { useRef } from 'react'
 
 const DashboardPaciente = () => {
 
   const { id } = useParams()
+  const textoLink = useRef()
   const kLinkDirecto = CryptoJS.AES.encrypt(id, process.env.REACT_APP_AESK)
+
+  const copiarLink = () => {
+    textoLink.current.select()
+    textoLink.current.setSelectionRange(0, 99999)
+    document.execCommand('copy')
+  }
 
   return (
     <div className="DashboardPaciente">
@@ -19,10 +29,17 @@ const DashboardPaciente = () => {
         <div className="DashboardPaciente__contenedor_link_para_compartir">
           <label>Link directo:</label>
           <input
+            ref={textoLink}
             className="DashboardPaciente__link_para_compartir"
             value={`${window.location.origin}?k=${encodeURIComponent(kLinkDirecto)}`}
           />
-          <button>Copiar</button>
+          <button
+            className="DashboardPaciente__boton_copiar_link"
+            title="Copiar link"
+            onClick={copiarLink}
+          >
+            <InlineIcon icon={iconoCopiar} />
+          </button>
         </div>
       </div>
       <nav className="DashboardPaciente__navegacion">
