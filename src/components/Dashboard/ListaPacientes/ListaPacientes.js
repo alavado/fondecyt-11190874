157bf7c56a1da27379f0581ad974e35fbf52ab19
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-08-17 18:12:32 -0400

    mejora presentacion de login

--- src/components/Dashboard/ListaPacientes/ListaPacientes.js
+++ src/components/Dashboard/ListaPacientes/ListaPacientes.js
@@ -1,13 +1,15 @@
 import { useQuery } from 'react-query'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { pacientes, statusPacienteActivo } from '../../../helpers/api'
+import { borraToken } from '../../../redux/ducks/login'
 import './ListaPacientes.css'
 
 const ListaPacientes = () => {
 
   const { jwt } = useSelector(state => state.login)
   const { isLoading, data, isError } = useQuery('pacientes', pacientes(jwt))
+  const dispatch = useDispatch()
 
   if (isLoading) {
     return 'Cargando...'
@@ -21,7 +23,7 @@ const ListaPacientes = () => {
 
   return (
     <div className="ListaPacientes">
-      <h1 className="ListaPacientes__titulo">Pacientes en estudio</h1>
+      <h1 className="ListaPacientes__titulo">Pacientes en estudio <button onClick={() => dispatch(borraToken())}>Cerrar sesión</button></h1>
       {pacientesActivos.map(p => (
         <div className="ListaPacientes__elemento" key={`fila-paciente-${p.id}`}>
           <Link to={`/paciente/${p.id}/consumo`}>
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,35 +2,44 @@ import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
 import { login } from '../../helpers/api'
 import { guardaToken } from '../../redux/ducks/login'
+import logoColor from '../../assets/TMB Color-02.svg'
 import './Login.css'
+import { useMutation } from 'react-query'
 
 const Login = () => {
   
   const { register, handleSubmit, formState: { errors } } = useForm()
   const dispatch = useDispatch()
-
-  const ingresar = ({ email, password }) => {
-    login(email, password)
-      .then(res => {
-        const { authorization: token } = res.headers
-        dispatch(guardaToken(token))
-      })
-  }
+  const { mutate, isLoading, isError } = useMutation(({ email, password }) => login(email, password), {
+    onSuccess: data => {
+      const { authorization: token } = data.headers
+      dispatch(guardaToken(token))
+    }
+  })
 
   return (
     <div className="Login">
-      <form className="Login__formulario" onSubmit={handleSubmit(ingresar)} >
-        <div className="Login__contenedor_campo">
-          <label htmlFor="email">E-mail</label>
-          <input {...register('email')} required={true} />
-          {errors.usuarioRequired && <span>Este campo es necesario</span>}
+      <form className="Login__formulario" onSubmit={handleSubmit(mutate)} >
+        <img src={logoColor} className="Login__logo" alt="Logo TMB" />
+        <div className="Login__contenedor_campos">
+          <div className="Login__contenedor_campo">
+            <label htmlFor="email">E-mail</label>
+            <input {...register('email')} required={true} />
+            {errors.usuarioRequired && <span>Este campo es necesario</span>}
+          </div>
+          <div className="Login__contenedor_campo">
+            <label htmlFor="password">Contraseña</label>
+            <input type="password" {...register('password')} required={true} />
+            {errors.passwordRequired && <span>Este campo es necesario</span>}
+          </div>
+          <button
+            className="Login__boton"
+            type="submit"
+            disabled={isLoading}
+          >
+            {!isLoading ? 'Ingresar' : 'Ingresando...'}
+          </button>
         </div>
-        <div className="Login__contenedor_campo">
-          <label htmlFor="password">Contraseña</label>
-          <input type="password" {...register('password')} required={true} />
-          {errors.passwordRequired && <span>Este campo es necesario</span>}
-        </div>
-        <button className="Login__boton" type="submit">Ingresar</button>
       </form>
     </div>
   )
