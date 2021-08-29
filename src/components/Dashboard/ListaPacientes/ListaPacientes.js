efc120b8833f3a0ebd510f422e931bb0aa44c6ee
Alejandro Lavado <alejandro.lvd@gmail.com>
2021-08-29 12:25:44 -0400

    mejora mucho tabla de pacientes

--- src/components/Dashboard/ListaPacientes/ListaPacientes.js
+++ src/components/Dashboard/ListaPacientes/ListaPacientes.js
@@ -1,15 +1,70 @@
+import { useMemo } from 'react'
 import { useQuery } from 'react-query'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link } from 'react-router-dom'
+import logoColor from '../../../assets/TMB Color-02.svg'
+import { useTable } from 'react-table'
 import { pacientes, statusPacienteActivo } from '../../../helpers/api'
 import { borraToken } from '../../../redux/ducks/login'
 import './ListaPacientes.css'
+import { differenceInYears, parse } from 'date-fns'
+import { useHistory } from 'react-router-dom'
 
 const ListaPacientes = () => {
 
   const { jwt } = useSelector(state => state.login)
-  const { isLoading, data, isError } = useQuery('pacientes', pacientes(jwt))
+  const { isLoading, data: dataPacientesAPI, isError } = useQuery('pacientes', pacientes(jwt))
   const dispatch = useDispatch()
+  const history = useHistory()
+
+  const data = useMemo(() => {
+    if (!dataPacientesAPI) {
+      return []
+    }
+    const data = dataPacientesAPI.data.data
+      .filter(p => p.attributes.status === statusPacienteActivo)
+      .map(paciente => ({
+        id: paciente.id,
+        nombre: paciente.attributes.nombre,
+        edad: `${differenceInYears(Date.now(), parse(paciente.attributes.f_nac, 'yyyy-MM-dd', new Date()))} años`,
+        fono: [paciente.attributes.fono1, paciente.attributes.fono2, paciente.attributes.fono3, paciente.attributes.fono4].filter(x => x).join(', '),
+        domicilio: paciente.attributes.domicilio,
+        email: paciente.attributes.email,
+        estado: paciente.attributes.status,
+        comentario: paciente.attributes.comentario
+      }))
+    return data
+  }, [dataPacientesAPI])
+
+  const columns = useMemo(() => [
+    {
+      Header: 'Nombre',
+      accessor: 'nombre',
+    },
+    {
+      Header: 'Edad',
+      accessor: 'edad',
+    },
+    {
+      Header: 'Teléfono',
+      accessor: 'fono',
+    },
+    {
+      Header: 'E-mail',
+      accessor: 'email',
+    },
+    {
+      Header: 'Comentario',
+      accessor: 'comentario',
+    },
+  ],[])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data })
 
   if (isLoading) {
     return 'Cargando...'
@@ -19,18 +74,43 @@ const ListaPacientes = () => {
     return 'Error cargando pacientes'
   }
 
-  const pacientesActivos = data.data.data.filter(p => p.attributes.status === statusPacienteActivo)
+  console.log(data)
 
   return (
     <div className="ListaPacientes">
-      <h1 className="ListaPacientes__titulo">Pacientes en estudio <button onClick={() => dispatch(borraToken())}>Cerrar sesión</button></h1>
-      {pacientesActivos.map(p => (
-        <div className="ListaPacientes__elemento" key={`fila-paciente-${p.id}`}>
-          <Link to={`/paciente/${p.id}/consumo`}>
-            {p.attributes.nombre}
-          </Link>
-        </div>
-      ))}
+      <div className="ListaPacientes__encabezado">
+        <h1 className="ListaPacientes__titulo"><img src={logoColor} className="ListaPacientes__logo" alt="Logo TMB" /> Pacientes en estudio (estado = 8)</h1>
+        <button onClick={() => dispatch(borraToken())}>Cerrar sesión</button>
+      </div>
+      <table className="ListaPacientes__tabla" {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+            prepareRow(row)
+            return (
+              <tr {...row.getRowProps()} onClick={() => history.push(`/paciente/${row.original.id}/consumo`)}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </div>
   )
 }
